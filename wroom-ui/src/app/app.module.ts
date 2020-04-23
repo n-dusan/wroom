5e51122c964c67e383b246158614f5f515b42b96
OljaSekulic <57363975+OljaSekulic@users.noreply.github.com>
2020-04-23 15:34:06 +0200

    Created form for vehicle registration.

--- wroom-ui/src/app/app.module.ts
+++ wroom-ui/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MaterialModule } from './modules/shared/material.module';
+import { MakingVehiclesFormComponent } from './modules/vehicles/making-vehicles-form/making-vehicles-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MakingVehiclesFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
