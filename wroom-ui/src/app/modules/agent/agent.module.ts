3eeb954863ee1b8379f14a6b0480847ff5622a56
anasvitlica29 <57350974+anasvitlica29@users.noreply.github.com>
2020-04-24 13:02:59 +0200

    Finalized home page for agent.

--- wroom-ui/src/app/modules/agent/agent.module.ts
+++ wroom-ui/src/app/modules/agent/agent.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AgentRoutingModule } from './agent-routing.module';
 import { HomeAgentComponent } from './home-agent/home-agent.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../shared/material.module';
 
 
 
@@ -19,13 +14,9 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     AgentRoutingModule, 
     SharedModule,
+    MaterialModule,
     FormsModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatFormFieldModule
+    ReactiveFormsModule
   ],
   exports: [
   ]
--- wroom-ui/src/app/modules/shared/material.module.ts
+++ wroom-ui/src/app/modules/shared/material.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
@@ -8,7 +14,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 const modules = [
   MatButtonModule,
   MatTableModule,
-  FlexLayoutModule
+  FlexLayoutModule,
+  MatCardModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule
 ]
 
 @NgModule({
