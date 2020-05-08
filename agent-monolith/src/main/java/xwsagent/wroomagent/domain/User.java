5f6ebb059fab05cb61a138458c2e4ce7dd67c0a5
anasvitlica29 <57350974+anasvitlica29@users.noreply.github.com>
2020-05-08 12:15:48 +0200

    Configured Spring Security

--- agent-monolith/src/main/java/xwsagent/wroomagent/domain/User.java
+++ agent-monolith/src/main/java/xwsagent/wroomagent/domain/User.java
@@ -1,6 +1,9 @@
 package xwsagent.wroomagent.domain;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -15,6 +18,10 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,7 +30,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 	// needs CRUD details, like deleted field and review data
 
 	@Id
@@ -60,7 +67,67 @@ public class User {
 
 	@Column
 	private boolean enabled;
+
+//	@Column
+//	private boolean locked;
+//	
+//	@Column
+//	private boolean expired;
 	
 	@Column
 	private boolean tokenExpired;
+
+	@Column
+	private Timestamp lastPasswordResetDate;
+	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		List<String> privileges = this.getPrivileges();
+		for (String privilege : privileges) {
+			authorities.add(new SimpleGrantedAuthority(privilege));
+		}
+		return authorities;
+	}
+
+	/*
+	 * RBAC privileges are 'same' as Spring Security authorities
+	 */
+	private List<String> getPrivileges() {
+		List<String> privileges = new ArrayList<>();
+		List<Privilege> collection = new ArrayList<>();
+		
+		for (Role role : roles) {
+			collection.addAll(role.getPrivileges());
+		}
+		for (Privilege item : collection) {
+			privileges.add(item.getName());
+		}
+		return privileges;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	// Indicates whether the user's account has expired. An expired account cannot be authenticated.
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	// Indicates whether the user is locked or unlocked. A locked user cannot be authenticated.
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	// Indicates whether the user's credentials (password) has expired. Expired
+	// credentials prevent authentication.
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
 }
---
+++ agent-monolith/src/main/java/xwsagent/wroomagent/security/auth/JwtAuthenticationRequest.java
@@ -0,0 +1,18 @@
+package xwsagent.wroomagent.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationRequest {
+
+	private String username;
+    private String password;
+	
+    
+}
