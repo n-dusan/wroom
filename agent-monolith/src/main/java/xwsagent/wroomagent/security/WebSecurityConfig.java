37b2661c208fff412520acc0601aff87e8753537
Dusan <nikolic.dusan.dey@gmail.com>
2020-05-16 01:38:21 +0200

    tlsing around my dude

--- agent-monolith/src/main/java/xwsagent/wroomagent/security/WebSecurityConfig.java
+++ agent-monolith/src/main/java/xwsagent/wroomagent/security/WebSecurityConfig.java
@@ -1,7 +1,14 @@
 package xwsagent.wroomagent.security;
 
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -68,24 +75,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //						BasicAuthenticationFilter.class);
 
 		//redirect http -> https
-	  // .requiresChannel().anyRequest().requiresSecure()
+	   //.requiresChannel().anyRequest().requiresSecure()
         ;
 
     }
 
 
-//	@Bean
-//	public TomcatServletWebServerFactory httpsRedirectConfig() {
-//		return new TomcatServletWebServerFactory() {
-//			@Override
-//			protected void postProcessContext(Context context) {
-//				SecurityConstraint securityConstraint = new SecurityConstraint();
-//				securityConstraint.setUserConstraint("CONFIDENTIAL");
-//				SecurityCollection collection = new SecurityCollection();
-//				collection.addPattern("/*");
-//				securityConstraint.addCollection(collection);
-//				context.addConstraint(securityConstraint);
-//			}
-//		};
-//	}
+
 }
