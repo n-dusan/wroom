33aab3c717bd09362ad98632208d583acca895e3
anasvitlica29 <57350974+anasvitlica29@users.noreply.github.com>
2020-05-18 11:18:32 +0200

    Re-added all classes

---
+++ auth-service/src/main/java/wroom/authservice/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package wroom.authservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import wroom.authservice.domain.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	@EntityGraph(value = "User.Roles.Permissions")
+	Optional<User> findByEmail(String email);
+	
+	boolean existsByEmail(String username);
+	
+	@Query("select u from User u where u.id = ?#{principal.id}")
+	User findCurrentUser();
+	
+	@Query("select u from User u where u.id != ?#{principal.id}")
+	List<User> findUsersExceptSelf();
+	
+}
