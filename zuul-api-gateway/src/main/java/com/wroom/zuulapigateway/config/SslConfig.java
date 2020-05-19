539e6f0452748a3a8eba2a211b0558feaf8a278a
Dusan <nikolic.dusan.dey@gmail.com>
2020-05-19 16:23:57 +0200

    removed unused config

--- zuul-api-gateway/src/main/java/com/wroom/zuulapigateway/config/SslConfig.java
+++ zuul-api-gateway/src/main/java/com/wroom/zuulapigateway/config/SslConfig.java
@@ -14,24 +14,5 @@ import java.security.NoSuchAlgorithmException;
 @Configuration
 public class SslConfig {
 
-//    @Bean
-//    public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
-//        DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
-//        System.setProperty("javax.net.ssl.keyStoreType", "PKCS12");
-//        System.setProperty("javax.net.ssl.keyStore", "src/main/resources/zuul-server.p12");
-//        System.setProperty("javax.net.ssl.keyStorePassword", "password");
-//        System.setProperty("javax.net.ssl.trustStoreType", "PKCS12");
-//        System.setProperty("javax.net.ssl.trustStore", "src/main/resources/truststore.p12");
-//        System.setProperty("javax.net.ssl.trustStorePassword", "password");
-//        EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
-//        builder.withClientName("655575225830");
-//        builder.withSystemSSLConfiguration();
-//        builder.withMaxTotalConnections(10);
-//        builder.withMaxConnectionsPerHost(10);
-//        args.setEurekaJerseyClient(builder.build());
-//        return args;
-//    }
-
-
 
 }
