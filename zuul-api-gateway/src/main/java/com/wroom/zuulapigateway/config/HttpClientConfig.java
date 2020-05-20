b724579322240dfcbd18a3c9f34b534bb035be1c
Dusan <nikolic.dusan.dey@gmail.com>
2020-05-20 22:50:21 +0200

    rollback on microservice tls

--- zuul-api-gateway/src/main/java/com/wroom/zuulapigateway/config/HttpClientConfig.java
+++ zuul-api-gateway/src/main/java/com/wroom/zuulapigateway/config/HttpClientConfig.java
@@ -8,6 +8,7 @@ import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,4 +21,26 @@ import java.security.cert.X509Certificate;
 @Configuration
 public class HttpClientConfig {
 
+    @Value("${server.ssl.trust-store}")
+    private File trustStore;
+    @Value("${server.ssl.trust-store-password}")
+    private String trustStorePassword;
+
+    @Bean
+    public CloseableHttpClient httpClient() throws Throwable {
+        SSLContext sslcontext = SSLContexts.custom()
+                .loadTrustMaterial(new File(trustStore.getPath()), trustStorePassword.toCharArray(),
+                        new TrustSelfSignedStrategy())
+                .build();
+
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                sslcontext,
+                new String[] { "TLSv1.2" },
+                null,
+                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+        CloseableHttpClient httpclient = HttpClients.custom()
+                .setSSLSocketFactory(sslsf)
+                .build();
+        return httpclient;
+    }
 }
