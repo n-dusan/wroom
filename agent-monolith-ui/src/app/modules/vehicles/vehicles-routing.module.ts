ac52ef1686357c9ba6b39a1832264fdcc2907bde
Dusan <nikolic.dusan.dey@gmail.com>
2020-05-22 22:17:41 +0200

    hibernate validator

--- agent-monolith-ui/src/app/modules/vehicles/vehicles-routing.module.ts
+++ agent-monolith-ui/src/app/modules/vehicles/vehicles-routing.module.ts
@@ -11,7 +11,6 @@ import { NewVehicleComponent } from './new-vehicle/new-vehicle.component';
 
 const routes: Routes = [
   { path: '',
-    //component: AAAAAAA,
     children: [
       { path: 'features-overview', component: FeaturesOverviewComponent },
       { path: 'new-model-type', component: NewModelTypeComponent},
