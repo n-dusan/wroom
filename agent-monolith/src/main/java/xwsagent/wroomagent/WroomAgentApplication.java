3fca8a69530499380984c39d0cd780bd21aa0de2
anasvitlica29 <57350974+anasvitlica29@users.noreply.github.com>
2020-05-24 16:52:04 +0200

    home page fix

--- agent-monolith/src/main/java/xwsagent/wroomagent/WroomAgentApplication.java
+++ agent-monolith/src/main/java/xwsagent/wroomagent/WroomAgentApplication.java
@@ -1,7 +1,5 @@
 package xwsagent.wroomagent;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.net.URISyntaxException;
 
 import org.springframework.boot.SpringApplication;
@@ -12,16 +10,16 @@ public class WroomAgentApplication {
 
 	public static void main(String[] args) throws URISyntaxException {
 
-		try {
-			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-			InputStream is = classloader.getResourceAsStream("somefile.txt"); 
-
-			while (is.available() > 0) {	//read content from protected file
-				System.out.print((char) is.read());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+//			InputStream is = classloader.getResourceAsStream("somefile.txt"); 
+//
+//			while (is.available() > 0) {	//read content from protected file
+//				System.out.print((char) is.read());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 
 		SpringApplication.run(WroomAgentApplication.class, args);
 		System.out.println("Agent monolith is running");
