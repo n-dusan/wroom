5beb68859f45a285f82bcd4d9290424f8b0a1110
Dusan <nikolic.dusan.dey@gmail.com>
2020-06-10 02:20:52 +0200

    data.sql

--- agent-monolith/src/main/resources/data.sql
+++ agent-monolith/src/main/resources/data.sql
@@ -2,22 +2,22 @@
 -- U create-drop rezimu, ukoliko sql skripta ponovo insertuje već unete vrednosti (MySQL ume da ne dropuje i redove unutar tabela)
 -- onda dodajte delete na tabelu koju želite da očistite, time se brišu svi redovi iz te tabele
 --------------------------------
-delete from rent_request_ads;
-delete from users;
-delete from user_roles;
-delete from permission;
-delete from roles;
-delete from role_permissions;
-delete from ad;
-delete from location;
-delete from rent_request;
-delete from vehicle;
-delete from model_type;
-delete from brand_type;
-delete from gearbox_type;
-delete from fuel_type;
-delete from body_type;
-delete from rate;
+-- delete from rent_request_ads;
+-- delete from users;
+-- delete from user_roles;
+-- delete from permission;
+-- delete from roles;
+-- delete from role_permissions;
+-- delete from ad;
+-- delete from location;
+-- delete from rent_request;
+-- delete from vehicle;
+-- delete from model_type;
+-- delete from brand_type;
+-- delete from gearbox_type;
+-- delete from fuel_type;
+-- delete from body_type;
+-- delete from rate;
 
 ------------- ROLE -----------------
 insert into roles(id, name)
